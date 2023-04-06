5fe2e7aa48507a74425f2d35d843779e48bf9134
Hyeonsu <gustn05233@naver.com>
2023-04-06 21:23:52 +0900

    사용자 비밀번호 변경 구현 완료

--- spring_webmail/target/spring_webmail-0.0.1-SNAPSHOT/WEB-INF/classes/static/js/register.js
+++ spring_webmail/target/spring_webmail-0.0.1-SNAPSHOT/WEB-INF/classes/static/js/register.js
@@ -1,5 +1,4 @@
 function checkPassword() {
-                    
                     var pw1 = document.getElementById( 'pw1' ).value;
                     var pw2 = document.getElementById( 'pw2' ).value;
                     if(pw1 != pw2){
