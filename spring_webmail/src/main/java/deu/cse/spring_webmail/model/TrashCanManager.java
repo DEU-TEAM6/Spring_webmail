68eaeea2979251082671ff2d062eca45a11fd120
Hyeonsu <gustn05233@naver.com>
2023-05-05 17:56:38 +0900

    휴지통 기능 개발 완료

---
+++ spring_webmail/src/main/java/deu/cse/spring_webmail/model/TrashCanManager.java
@@ -0,0 +1,222 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deu.cse.spring_webmail.model;
+
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author PC
+ */
+@Slf4j
+public class TrashCanManager {
+
+    private String url;
+    private String id;
+    private String pw;
+    private String driver;
+
+    public TrashCanManager() {
+        log.debug("TrashCanManager(): Driver = {}", driver);
+    }
+
+    public TrashCanManager(String url, String id, String pw, String driver) {
+        this.url = url;
+        this.id = id;
+        this.pw = pw;
+        this.driver = driver;
+        log.debug("AddrBookManager(): Driver = {}", driver);
+    }
+
+    public List<TrashCanRow> getAllRows(String userid) {
+        List<TrashCanRow> dataList = new ArrayList<>();
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, id, pw);
+            stmt = conn.createStatement();
+            String sql = "SELECT * FROM TRASHCAN";
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                if (userid.equals(rs.getString("repository_name"))) {
+                    String message_name = rs.getString("message_name");
+                    String repository_name = rs.getString("repository_name");
+                    String message_state = rs.getString("message_state");
+                    String error_message = rs.getString("error_message");
+                    String sender = rs.getString("sender");
+                    sender = sender.substring(0, sender.indexOf("@"));
+                    String recipients = rs.getString("recipients");
+                    String remote_host = rs.getString("remote_host");
+                    String remote_addr = rs.getString("remote_addr");
+                    Blob message_body = rs.getBlob("message_body");
+                    Blob message_attributes = rs.getBlob("message_attributes");
+                    Date last_updated = rs.getDate("last_updated");
+                    dataList.add(new TrashCanRow(message_name, repository_name, message_state, error_message, sender, recipients,
+                            remote_host, remote_addr, message_body, message_attributes, last_updated));
+                }
+            }
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+
+        } catch (Exception ex) {
+            log.error("오류가 발생했습니다. (발생 오류: {})", ex.getMessage());
+        }
+        return dataList;
+    }
+
+    public boolean deleteRow(String message_name) {
+        boolean result = false;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, id, pw);
+            String sql = "DELETE FROM TRASHCAN WHERE MESSAGE_NAME = ?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, message_name);
+
+            pstmt.executeUpdate();
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+            result = true;
+        } catch (Exception ex) {
+            log.error("deleteRow 오류가 발생했습니다. (발생 오류: {})", ex.getMessage());
+        }
+        return result;
+    }
+
+    public boolean insertRow(String message_name) {
+        boolean result = false;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        List<Object> rowList = selectRow(message_name);
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, id, pw);
+            String sql = "INSERT INTO INBOX VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+            pstmt = conn.prepareStatement(sql);
+            for (int i = 0; i < 11; i++) {
+                Object obj = rowList.get(i);
+                if (obj == null) {
+                    switch (i) {
+                        case 8:
+                        case 9:
+                            pstmt.setNull(i + 1, java.sql.Types.BLOB);
+                            break;
+                        case 10:
+                            pstmt.setNull(i + 1, java.sql.Types.DATE);
+                            break;
+                        default:
+                            pstmt.setNull(i + 1, java.sql.Types.VARCHAR);
+                    }
+                } else {
+                    switch (i) {
+                        case 8:
+                        case 9:
+                            pstmt.setBlob(i + 1, (Blob) obj);
+                            break;
+                        case 10:
+                            pstmt.setDate(i + 1, (Date) obj);
+                            break;
+                        default:
+                            pstmt.setString(i + 1, (String) obj);
+                    }
+                }
+            }
+            pstmt.executeUpdate();
+
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+            deleteRow(message_name); // 휴지통에서 지우기
+            result = true;
+        } catch (Exception ex) {
+            log.error("insertRow 오류가 발생했습니다. (발생 오류: {})", ex.getMessage());
+        }
+        return result;
+    }
+
+    public List<Object> selectRow(String message_name) {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        List<Object> row = new ArrayList<>();
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, id, pw);
+            String sql = "SELECT * FROM TRASHCAN";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                if (message_name.equals(rs.getString("message_name"))) {
+                    row.add(rs.getString("message_name"));
+                    row.add(rs.getString("repository_name"));
+                    row.add(rs.getString("message_state"));
+                    row.add(rs.getString("error_message"));
+                    row.add(rs.getString("sender"));
+                    row.add(rs.getString("recipients"));
+                    row.add(rs.getString("remote_host"));
+                    row.add(rs.getString("remote_addr"));
+                    row.add(rs.getBlob("message_body"));
+                    row.add(rs.getBlob("message_attributes"));
+                    row.add(rs.getDate("last_updated"));
+                }
+            }
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (Exception ex) {
+            log.error("오류가 발생했습니다. (발생 오류: {})", ex.getMessage());
+        }
+        return row;
+    }
+}
---
+++ spring_webmail/target/spring_webmail-0.0.1-SNAPSHOT/WEB-INF/views/trash_can.jsp
@@ -0,0 +1,66 @@
+<%-- 
+    Document   : trash_can
+    Created on : 2023. 5. 2., 오후 6:31:21
+    Author     : Jung-HyeonSu
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+
+<!DOCTYPE html>
+
+<!-- 제어기에서 처리하면 로직 관련 소스 코드 제거 가능!
+<jsp:useBean id="pop3" scope="page" class="deu.cse.spring_webmail.model.Pop3Agent" />
+<%
+    pop3.setHost((String) session.getAttribute("host"));
+    pop3.setUserid((String) session.getAttribute("userid"));
+    pop3.setPassword((String) session.getAttribute("password"));
+%>
+-->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>휴지통 화면</title>
+        <link type="text/css" rel="stylesheet" href="css/main_style.css" />
+        <script>
+            <c:if test="${!empty msg}">
+            alert("${msg}");
+            </c:if>
+        </script>
+    </head>
+    <body>
+        <%@include file="header.jspf"%>
+
+        <div id="sidebar">
+            <jsp:include page="sidebar_menu.jsp" />
+        </div>
+
+
+        <!-- 메시지 삭제 링크를 누르면 바로 삭제되어 실수할 수 있음. 해결 방법은? -->
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>보낸 사람</th>
+                    <th>보낸 날짜</th>
+                    <th>복원</th>
+                    <th>영구삭제</th>
+                </tr>
+            </thead>
+            <tbody>
+                <c:forEach var="row" items="${trashcanList}">
+                    <tr>
+                        <td>${row.sender}</td>
+                        <td>${row.last_updated}</td>
+                        <td><a href="trashcan_mail.do?message_name=${row.message_name}&type=restore"> 복원 </a></td>
+                        <td><a href="trashcan_mail.do?message_name=${row.message_name}&type=delete"> 영구삭제 </a></td>
+                    </tr>
+                </c:forEach>
+            </tbody>
+        </table>
+
+        <%@include file="footer.jspf"%>
+    </body>
+</html>
