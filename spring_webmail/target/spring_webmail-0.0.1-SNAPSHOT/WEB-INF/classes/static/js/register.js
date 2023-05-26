371e3d1acc908bdf30fb0d08773309fc455fa133
KingMugyeong <92977544+KingMugyeong@users.noreply.github.com>
2023-05-26 20:44:36 +0900

    내용비교추가

--- spring_webmail/target/spring_webmail-0.0.1-SNAPSHOT/WEB-INF/classes/static/js/register.js
+++
@@ -1,8 +0,0 @@
-function checkPassword() {
-    var pw1 = document.querySelector('#pw1').value;
-    var pw2 = document.querySelector('#pw2').value;
-    if (pw1 !== pw2) {
-        alert("비밀번호가 일치하지 않습니다.");
-        return false;
-    }
-}
